9e114c49ec550b973cd203d26f2d0cbce0a10b6f
MizginYildirak <mizgin_yldrak@hotmail.com>
2025-02-28 18:29:33 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const clearBtn = document.getElementById("clear");
+const sizeElement = document.getElementById
